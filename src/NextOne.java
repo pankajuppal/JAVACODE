58dfec8f7fc26ec8d69b57322fef9e467b8c64ec
Pankaj <pankaj.uppal@gmail.com>
2024-08-23 16:56:46 -0500

    Initial Changes

---
+++ src/NextOne.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class NextOne {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter Pricipal Amount:");
+            int Principal = scanner.nextInt();
+            System.out.print("Enter the interest rate:");
+            byte rate = scanner.nextByte();
+            System.out.println("Interest to pay is : " + (Principal*rate)/100);
+
+        }
+    }
+
