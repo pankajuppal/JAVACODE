6baa775a7032d16e9e6f714ab63ca0c5cf017c6d
Pankaj <pankaj.uppal@gmail.com>
2024-08-23 16:39:45 -0500

    Initial Changes

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import org.w3c.dom.ls.LSInput;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Pricipal Amount:");
+        int Principal = scanner.nextInt();
+        System.out.print("Enter the interest rate:");
+        byte rate = scanner.nextByte();
+        System.out.println("Interest to pay is:" + (Principal*rate)/100);
+
+        }
+    }
