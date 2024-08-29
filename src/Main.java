52746edc3191f2db5332fb109d8822ebb94c6b85
Pankaj <pankaj.uppal@gmail.com>
2024-08-29 14:39:57 -0500

    testing

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,6 @@ public class Main {
         System.out.print("Enter the interest rate:");
         byte rate = scanner.nextByte();
         System.out.println("Interest to pay is : " + (Principal*rate)/100);
-
+        //Testing
         }
     }
